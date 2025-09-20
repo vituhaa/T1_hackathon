7f2ab68b9ae7530a00e7dce78d04555a985269ef
Sonya Novikova <sona.nn@yandex.ru>
2025-09-20 17:52:45 +0300

    Applied employee route

--- backend/server.js
+++ backend/server.js
@@ -13,17 +13,17 @@ app.get('/api/message', (req, res) => {
 });
 
 app.use('/hr', express.static(path.join(__dirname, '../hr-portal/dist')));
-//app.use('/employee', express.static(path.join(__dirname, '../employee-portal/dist')));
+app.use('/employee', express.static(path.join(__dirname, '../empl-portal/dist')));
 
 app.use('/hr', (req, res) => {
   res.sendFile(path.join(__dirname, '../hr-portal/dist/index.html'));
 });
 
-// app.use('/employee', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../employee-portal/dist/index.html'));
-// });
+app.use('/employee', (req, res) => {
+  res.sendFile(path.join(__dirname, '../empl-portal/dist/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`HR панель: http://localhost:${port}/hr`);
-  //console.log(`Employee панель: http://localhost:${port}/employee`);
+  console.log(`Employee панель: http://localhost:${port}/employee`);
 });
